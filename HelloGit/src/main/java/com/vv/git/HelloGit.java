9a23f26ced3c619aa9bb8bd2bd1ad71f1dd0b5ec
tom_glb <123123@ii.com>
2020-07-11 18:50:57 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/vv/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.vv.git;
+
+/**
+ * @author vv
+ * @create 2020-07-11 18:37
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
